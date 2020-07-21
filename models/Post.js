b13767967793f92de8a385860aa00888e57bf7cf
whiteiswhite <62106279+whiteiswhite@users.noreply.github.com>
2020-07-21 19:30:05 +0200

    Add files via upload

---
+++ models/Post.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const post = {
+   title: {
+      type: String,
+      required: true
+   },
+   body: {
+      type: String,
+      required: true
+   },
+   tag: {
+      type: String,
+      required: true
+   },
+   author: {
+      type: String,
+      required: true
+   },
+   date: {
+      type: Date,
+      default: Date.now
+   }
+};
+
+const PostSchema = new Schema(post);
+
+module.exports = mongoose.model("posts", PostSchema);
